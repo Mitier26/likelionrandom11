3ad78229d80965f1cf844ccafb5f5623bf4765ba
Molkang <mitier26@gmail.com>
2025-01-21 09:32:32 +0900

    더하기

---
+++ Test1.cs
@@ -0,0 +1,15 @@
+class Test1
+{
+    public static void Main()
+    {
+        System.Console.WriteLine("Hello 랜덤 11 조");
+
+        print(Add(1,2));
+           
+    }
+
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
